d99f6e29a6f1ca73e4610ae5e46ba80e8a1d2a67
Yash Barot <yashce04@gmail.com>
2016-09-19 21:56:24 +0530

    Added config

---
+++ config/dbbackup.php
@@ -0,0 +1,45 @@
+<?php
+
+return [
+
+    /*
+	|--------------------------------------------------------------------------
+	| App name
+	|--------------------------------------------------------------------------
+	|
+	| App name will be used in Email template and Statestics screen
+	|--------------------------------------------------------------------------
+	*/
+    'app_name' => env('APP_NAME', ''),
+
+    /*
+	|--------------------------------------------------------------------------
+	| Database Configurations
+	|--------------------------------------------------------------------------
+	|
+	| Mysql is only currently supported
+	|--------------------------------------------------------------------------
+	*/
+    'database' => [
+        "username" => env('DB_USERNAME', 'root'),
+        "password" => env('DB_PASSWORD', 'root'),
+        "host"     => env('DB_HOST', 'localhost'),
+        "name" => env('DB_DATABASE', 'db_name')
+    ],
+
+    /*
+	|--------------------------------------------------------------------------
+	| Email Configurations
+	|--------------------------------------------------------------------------
+	|
+	| Basic email configuration here and rest will taken care from your .env
+	| file i.e. mail_driver, mail_host etc.
+	|--------------------------------------------------------------------------
+	*/
+    'mail' => [
+        'from'     => env('MAIL_ADDRESS', ''),
+        'to'       => env('MAIL_TO', ''),
+        'alias'    => env('MAIL_NAME', ''),
+        'template' => 'emails.db_backup_email'
+    ]
+];
